b15e399ee7534d53879c4a9381279cbaf8f836e1
Noelia Luque <n.luque91@gmail.com>
2019-12-02 12:35:43 +0100

    add styles and list from js

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -2,6 +2,7 @@
 
 const searchBtn = document.querySelector(".js-button");
 let seriesInput = document.querySelector(".js-input");
+const seriesList = document.querySelector(".js-series-container");
 
 let series = [];
 
@@ -12,13 +13,30 @@ function getServerData() {
     })
     .then(function(serverData) {
       series = serverData;
-      console.log(series);
+      paintSeries();
     })
     .catch(function(err) {
       console.log("Error al traer los datos del servidor");
     });
 }
 getServerData();
+
+///FUNTION PAINTSERIES\\\
+
+function paintSeries() {
+  let htmlCode = "";
+  //   for (let i = 0; i < series.length; i++) {
+  for (const serie of series) {
+    htmlCode += `<li class="js-serieElement serie__element">`;
+    htmlCode += `<img src="${serie.show.image.medium}" class="js-serie-image serie__image"/>`;
+    htmlCode += `<h3 class="js-serie-title serie__title">${serie.show.name}</h3>`;
+    htmlCode += `</li>`;
+    console.log(serie.show.name);
+    console.log(serie.show.image.medium);
+  }
+  seriesList.innerHTML = htmlCode;
+}
+// }
 
 ///FUNCTION MATHERFUCKER\\\
 
