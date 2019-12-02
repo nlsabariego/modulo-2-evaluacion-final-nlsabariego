9dfd6ca49ea07a36ec68f029937481dc2f2c0529
Noelia Luque <n.luque91@gmail.com>
2019-12-02 10:08:28 +0100

    Get server data

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,5 +1,33 @@
-'use strict';
+"use strict";
 
-console.log('>> Ready :)');
+const searchBtn = document.querySelector(".js-button");
+let seriesInput = document.querySelector(".js-input");
+
+let series = [];
+
+function getServerData() {
+  fetch(`http://api.tvmaze.com/search/shows?q=${seriesInput.value}`)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(serverData) {
+      series = serverData;
+      console.log(series);
+    })
+    .catch(function(err) {
+      console.log("Error al traer los datos del servidor");
+    });
+}
+getServerData();
+
+///FUNCTION MATHERFUCKER\\\
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  getServerData();
+  console.log(seriesInput.value);
+}
+
+searchBtn.addEventListener("click", handleFormSubmit);
 
 //# sourceMappingURL=main.js.map
